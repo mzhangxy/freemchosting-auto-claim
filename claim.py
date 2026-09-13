f75844b1975d114c7bf63e893e66b53938b9f5a7
mzhangxy <96800168+mzhangxy@users.noreply.github.com>
2026-09-13 15:43:44 +0800

    Update claim.py

--- claim.py
+++ claim.py
@@ -46,7 +46,7 @@ REWARDS_URL = DASH + '/rewards'
 
 USERNAME = os.environ.get('MC_USERNAME', '')
 PASSWORD = os.environ.get('MC_PASSWORD', '')
-PROXY = os.environ.get('CLAIM_PROXY', '')  # 可选住宅代理, 如 http://user:pass@ip:port
+PROXY = os.environ.get('PROXY', '') 
 MAX_ROUNDS = int(os.environ.get('MAX_ROUNDS', '3') or '3')
 LOOT_MAX = int(os.environ.get('LOOT_MAX_SECONDS', '600') or '600')
 DRY_RUN = os.environ.get('DRY_RUN', '0') == '1'
